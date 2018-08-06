634533c0db0d4a4225202c80a2b0f67c028382ff
dinesh <dinesh1042@gmail.com>
2018-08-06 17:24:33 +0530

    CI: install bundle

--- lib/stokla.rb
+++ lib/stokla.rb
@@ -1,4 +1,4 @@
-require 'logger'
+require "logger"
 require_relative "stokla/version"
 require_relative "stokla/pool"
 
@@ -7,14 +7,14 @@ module Stokla
     attr_accessor :logger, :delete_after_completion, :_pool
     attr_accessor :schema, :table_name, :max_attempts
 
-    DEFAULT_OPTS = { schema: 'public', table_name: 'jobs', max_attempts: 5 }
+    DEFAULT_OPTS = {schema: "public", table_name: "jobs", max_attempts: 5}
 
-    def configure(options={})
+    def configure(options = {})
       if block_given?
-        DEFAULT_OPTS.each {|k,v| self.send("#{k}=", v) }
+        DEFAULT_OPTS.each { |k, v| self.send("#{k}=", v) }
         yield(self)
       else
-        DEFAULT_OPTS.merge(options).each {|k,v| self.send("#{k}=", v) }
+        DEFAULT_OPTS.merge(options).each { |k, v| self.send("#{k}=", v) }
       end
     end
 
@@ -23,24 +23,25 @@ module Stokla
     end
 
     def logger
-      @logger ||= 
+      @logger ||=
         if defined?(Rails)
           Rails.logger
         else
-          logger = Logger.new(STDOUT, log_level: Logger::INFO)
-          logger.progname = 'stok'
+          logger = Logger.new(STDOUT)
+          logger.level = Logger::INFO
+          logger.progname = "stok"
           logger
         end
     end
 
     def pool
-      self._pool.tap{|p| raise "Stokla.pool is not set." unless p }
+      self._pool.tap { |p| raise "Stokla.pool is not set." unless p }
     end
 
     def pool=(apool)
-      self._pool = 
+      self._pool =
         case apool.class.to_s
-        when 'ActiveRecord::ConnectionAdapters::ConnectionPool'
+        when "ActiveRecord::ConnectionAdapters::ConnectionPool"
           Adapter::ActiveRecord.new(apool)
         when "Sequel::Postgres::Database"
           Adapter::Sequel.new(apool)
@@ -49,7 +50,7 @@ module Stokla
         when "Pond"
           Adapter::Pond.new(apool)
         else
-          if ENV['RACK_ENV'] == 'test'
+          if ENV["RACK_ENV"] == "test"
             apool
           else
             raise "Usupported pool type: #{apool.to_s}"
@@ -59,5 +60,5 @@ module Stokla
   end
 end
 
-require_relative 'stokla/queue'
-require_relative 'stokla/job'
+require_relative "stokla/queue"
+require_relative "stokla/job"
